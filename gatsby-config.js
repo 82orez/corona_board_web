4c8e1ac651c87b63d0ef7bbd519e92b80abbc598
82orez <82orez@naver.com>
2023-07-25 11:27:35 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Coronaboard Web`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
